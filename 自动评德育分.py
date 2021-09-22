6a61ca19b1586c27ac3219fe186bb15398de3d34
torchlegend <1306087652@qq.com>
2021-09-22 17:32:46 +0800

    Create 自动评德育分.py

---
+++ 自动评德育分.py
@@ -0,0 +1,13 @@
+import openpyxl
+import random
+
+workbook = openpyxl.load_workbook("a.xlsx")
+
+worksheet = workbook["Sheet1"]
+print(worksheet)
+
+random_score = [random.randint(88, 92) for _ in range(33)]
+print(random_score)
+for i in range(len(random_score)):
+    worksheet.cell(i + 2, 3, random_score[i])
+workbook.save('德育学生打分表.xlsx')
