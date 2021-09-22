452356ca4f9a46e85539348bed2a4f42cef67e6d
torchlegend <1306087652@qq.com>
2021-09-22 17:32:56 +0800

    Update 自动评德育分.py

--- 自动评德育分.py
+++ 自动评德育分.py
@@ -8,6 +8,8 @@ print(worksheet)
 
 random_score = [random.randint(88, 92) for _ in range(33)]
 print(random_score)
+
 for i in range(len(random_score)):
     worksheet.cell(i + 2, 3, random_score[i])
+
 workbook.save('德育学生打分表.xlsx')
